548518a1afc4d2324abe5388df0158da485d0984
yueqian <yueqian@meili-inc.com>
2017-02-15 20:03:32 +0800

    yueqian:增加接口

---
+++ library/src/main/java/com/kaneki/xchatmessageview/listener/OnItemClickListener.java
@@ -0,0 +1,13 @@
+package com.kaneki.xchatmessageview.listener;
+
+import com.kaneki.xchatmessageview.holder.XViewHolder;
+
+/**
+ * @author yueqian
+ * @Desctription
+ * @date 2017/1/19
+ * @email yueqian@mogujie.com
+ */
+public interface OnItemClickListener {
+    void onClick();
+}
--- sample/src/main/java/com/kaneki/sample/MainActivity.java
+++ sample/src/main/java/com/kaneki/sample/MainActivity.java
@@ -5,11 +5,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
