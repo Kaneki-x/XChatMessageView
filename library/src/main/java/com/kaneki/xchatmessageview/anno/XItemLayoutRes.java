7d6f66fdbfd47c0571a8cd43ee9b60ba9c115459
yueqian <yueqian@meili-inc.com>
2017-02-17 18:40:15 +0800

    yueqian:增加注解

---
+++ library/src/main/java/com/kaneki/xchatmessageview/anno/XItemLayoutRes.java
@@ -0,0 +1,23 @@
+package com.kaneki.xchatmessageview.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 配置消息列表Item布局资源
+ *
+ * @author kaneki
+ * @version 16/11/24
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface XItemLayoutRes {
+
+    /**
+     * @return 待添加的Item布局资源类型
+     */
+    int[] value();
+
+}
---
+++ library/src/main/java/com/kaneki/xchatmessageview/anno/XItemLayoutResResolver.java
@@ -0,0 +1,35 @@
+package com.kaneki.xchatmessageview.anno;
+
+import android.support.annotation.NonNull;
+
+import com.kaneki.xchatmessageview.base.XMessageAdapter;
+
+/**
+ * {@link XItemLayoutRes}注解解析器<br/>
+ * 用来解析并创建Item布局实例<br/>
+ *
+ * @author Kaneki
+ * @version 16/11/24
+ */
+public class XItemLayoutResResolver {
+
+    /**
+     * 解析并创建布局资源实例
+     *
+     * @param xMessageAdapter      待解析的实例对象
+     * @return 布局Res实例集合
+     */
+    public static int[] resolve(@NonNull XMessageAdapter xMessageAdapter) {
+        Class<?> cls = xMessageAdapter.getClass();
+        int[] resIds;
+
+        XItemLayoutRes xItemLayoutRes = cls.getAnnotation(XItemLayoutRes.class);
+        // 根据注解配置信息生成底部菜单待填充项实例
+        resIds = xItemLayoutRes.value();
+
+        return resIds;
+    }
+
+
+
+}
